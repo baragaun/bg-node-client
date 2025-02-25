e316df9ad8a953a8d9e9a57cff74c4c8dc50757b
shadowbrush <github@shadowbrush.com>
2025-02-25 11:42:17 -0800

    bg-code-gen

--- tools/bg-code-gen/copy-project.sh
+++ tools/bg-code-gen/copy-project.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if [[ "$CHANNELS_ROOT" = "" ]]; then
+if [[ "$BG_CHANNELS_WEB_CLIENT_ROOT" = "" ]]; then
   projectRoot="../bg-channels-web-client"
 else
-  projectRoot="$CHANNELS_ROOT"
+  projectRoot="$BG_CHANNELS_WEB_CLIENT_ROOT"
 fi
 
 rsync -avP --delete "$projectRoot/tools/bg-code-gen/" "./src/projects/bg-channels-web-client/"
