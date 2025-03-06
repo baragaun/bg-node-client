931d6aab9b617bf0f6a24a3457d0a6157ca5dc90
shadowbrush <github@shadowbrush.com>
2025-03-06 13:41:28 -0800

    Fixing export

--- src/helpers/defaults.ts
+++ src/helpers/defaults.ts
@@ -1,4 +1,5 @@
-import { CachePolicy, QueryOptions } from '../types/index.js';
+import { CachePolicy } from '../types/enums.js';
+import { QueryOptions } from '../types/index.js';
 
 export const defaultQueryOptions: QueryOptions = {
   cachePolicy: CachePolicy.cacheFirst,
