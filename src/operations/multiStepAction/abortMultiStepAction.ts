5e3a1d502e971830c443ebb7abc6288fa0ae3fc8
shadowbrush <github@shadowbrush.com>
2025-03-20 18:56:04 -0700

    fix

--- src/operations/multiStepAction/abortMultiStepAction.ts
+++ src/operations/multiStepAction/abortMultiStepAction.ts
@@ -1,9 +1,12 @@
 import libData from '../../helpers/libData.js';
+import logger from '../../helpers/logger.js';
 
 const abortMultiStepAction = (actionId: string): boolean => {
   const run = libData.multiStepActionRun(actionId);
 
   if (!run) {
+    logger.warn('abortMultiStepAction: MultiStepActionRun not found, cannot abort',
+      { actionId });
     return false;
   }
 
